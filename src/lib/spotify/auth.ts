7cd468b368b0aabe5ea205f29d33fc7c23be2d11
Elias <42189915+elias-knodel@users.noreply.github.com>
2024-12-04 10:43:16 +0100

    feat: replace hardcoded with dynamic url base

--- src/lib/spotify/auth.ts
+++ src/lib/spotify/auth.ts
@@ -1,4 +1,5 @@
 import { PUBLIC_SPOTIFY_CLIENT_ID } from '$env/static/public';
+import { base } from '$app/paths';
 
 export async function redirectToAuthCodeFlow() {
 	const verifier = generateCodeVerifier(128);
@@ -9,7 +10,7 @@ export async function redirectToAuthCodeFlow() {
 	const params = new URLSearchParams();
 	params.append('client_id', PUBLIC_SPOTIFY_CLIENT_ID);
 	params.append('response_type', 'code');
-	params.append('redirect_uri', 'http://localhost:5173/callback');
+	params.append('redirect_uri', `${base}/callback`);
 	params.append('scope', 'user-top-read user-read-private user-read-email');
 	params.append('code_challenge_method', 'S256');
 	params.append('code_challenge', challenge);
@@ -24,7 +25,7 @@ export async function getAccessToken(code: string): Promise<string> {
 	params.append('client_id', PUBLIC_SPOTIFY_CLIENT_ID);
 	params.append('grant_type', 'authorization_code');
 	params.append('code', code);
-	params.append('redirect_uri', 'http://localhost:5173/callback');
+	params.append('redirect_uri', `${base}/callback`);
 	params.append('code_verifier', verifier!);
 
 	const result = await fetch('https://accounts.spotify.com/api/token', {
